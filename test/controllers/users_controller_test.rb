eb109a70886390795dfc0f2345516f913d6d9f9a
k-toriik-torii <k-torii@rhizome-e.com>
2021-12-29 09:34:44 +0900

    Finish user signup

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -5,10 +5,4 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     get users_new_url
     assert_response :success
   end
-
-  test "should get show" do
-    get users_show_url
-    assert_response :success
-  end
-
 end
